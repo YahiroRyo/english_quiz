b1f9c0be98b78e21372845a1e0f864333cb6ffd2
YahiroRyo <honakafn@gmail.com>
2023-06-10 14:47:31 +0900

    DEFAULT_USER_ID変更

--- api/src/User/Domain/Entity/InitUserConstants.php
+++ api/src/User/Domain/Entity/InitUserConstants.php
@@ -4,6 +4,6 @@ namespace Eng\User\Domain\Entity;
 
 class InitUserConstants
 {
-    public const DEFAULT_USER_ID    = 1;
+    public const DEFAULT_USER_ID    = 0;
     public const DEFAULT_USER_TOKEN = '';
 }
