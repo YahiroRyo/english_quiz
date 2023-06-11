f27c95d1cc754808f303c0e1a1a339756589a7b3
YahiroRyo <honakafn@gmail.com>
2023-06-11 22:41:37 +0900

    Font周りを調整

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,10 +1,17 @@
 import { Provider } from "jotai";
 import { Html, Head, Main, NextScript } from "next/document";
+import { Noto_Sans_JP } from "@next/font/google";
+
+const notojp = Noto_Sans_JP({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Document() {
   return (
     <Provider>
-      <Html lang="ja">
+      <Html className={notojp.className} lang="ja">
         <Head />
         <body>
           <Main />
