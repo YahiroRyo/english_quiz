017005131ea095e1655ae9df684249ba146727b0
YahiroRyo <honakafn@gmail.com>
2023-06-11 22:44:47 +0900

    _app.tsxにフォントの設定を移動

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import type { AppProps } from 'next/app'
-import '../styles/global.scss'
+import type { AppProps } from "next/app";
+import { Noto_Sans_JP } from "@next/font/google";
+import "../styles/global.scss";
+
+const notojp = Noto_Sans_JP({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component className={notojp.className} {...pageProps} />;
 }
--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,17 +1,10 @@
 import { Provider } from "jotai";
 import { Html, Head, Main, NextScript } from "next/document";
-import { Noto_Sans_JP } from "@next/font/google";
-
-const notojp = Noto_Sans_JP({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export default function Document() {
   return (
     <Provider>
-      <Html className={notojp.className} lang="ja">
+      <Html lang="ja">
         <Head />
         <body>
           <Main />
